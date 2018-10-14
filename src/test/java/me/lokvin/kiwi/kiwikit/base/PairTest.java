8e7c4257f763e66cf722a1599a3e6c18dedb7554
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-10-14 20:23:12 +0800

    update

--- src/test/java/me/lokvin/kiwi/kiwikit/base/PairTest.java
+++ src/test/java/me/lokvin/kiwi/kiwikit/base/PairTest.java
@@ -10,6 +10,8 @@ public class PairTest {
     public void pairTest() {
         Pair<String, Integer> pair1 = Pair.of("mary", 24);
         Pair<String, Integer> pair2 = Pair.of("mary", 30);
+        Pair<String, Integer> pair3 = Pair.of("mary", 24);
         Assert.assertNotEquals(pair1, pair2);
+        Assert.assertEquals(pair1, pair3);
     }
 }
