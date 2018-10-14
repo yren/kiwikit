7033ceb468f78394fa598b1ac619b07b26dbdeec
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-10-14 23:22:29 +0800

    update

--- src/test/java/me/lokvin/kiwi/kiwikit/base/TripleTest.java
+++ src/test/java/me/lokvin/kiwi/kiwikit/base/TripleTest.java
@@ -10,7 +10,9 @@ public class TripleTest {
     public void tripleTest() {
         Triple<String, String, Integer> t1 = Triple.of("mary", "female", 20);
         Triple<String, String, Integer> t2 = Triple.of("tom", "male", 23);
+        Triple<String, String, Integer> t3 = Triple.of("mary", "female", 20);
 
         Assert.assertNotEquals(t1, t2);
+        Assert.assertEquals(t1, t3);
     }
 }
