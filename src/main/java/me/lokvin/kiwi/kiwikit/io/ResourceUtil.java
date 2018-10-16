e5f249e28e5c9b356e459e6a8efc7726d63214d2
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-10-16 16:08:56 +0800

    update

--- src/main/java/me/lokvin/kiwi/kiwikit/io/ResourceUtil.java
+++ src/main/java/me/lokvin/kiwi/kiwikit/io/ResourceUtil.java
@@ -1,14 +1,18 @@
 package me.lokvin.kiwi.kiwikit.io;
 
 import com.google.common.io.Resources;
+import me.lokvin.kiwi.kiwikit.reflect.ClassLoaderUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 
-public class URLResourceUtil {
+public class ResourceUtil {
 
     public static URL asUrl(String resourceName) {
         return Resources.getResource(resourceName);
@@ -19,11 +23,11 @@ public class URLResourceUtil {
     }
 
     public static InputStream asStream(String resourceName) throws IOException {
-        return URLResourceUtil.asUrl(resourceName).openStream();
+        return ResourceUtil.asUrl(resourceName).openStream();
     }
 
     public static InputStream asStream(Class<?> contextClass, String resourceName) throws IOException {
-        return URLResourceUtil.asUrl(contextClass, resourceName).openStream();
+        return ResourceUtil.asUrl(contextClass, resourceName).openStream();
     }
 
     public static String toString(String resourceName) throws IOException {
@@ -40,6 +44,24 @@ public class URLResourceUtil {
 
     public static List<String> toLines(Class<?> contextClass, String resourceName) throws IOException {
         return Resources.readLines(Resources.getResource(contextClass, resourceName), StandardCharsets.UTF_8);
+    }
+
+    public static List<URL> getResourcesQuietly(String resourceName) {
+        return getResourcesQuietly(resourceName, ClassLoaderUtil.getDefaultClassLoader());
+    }
+
+    private static List<URL> getResourcesQuietly(String resourceName, ClassLoader classLoader) {
+        try {
+            Enumeration<URL> urls = classLoader.getResources(resourceName);
+            List<URL> list = new ArrayList<URL>(10);
+            while (urls.hasMoreElements()) {
+                list.add(urls.nextElement());
+            }
+            return list;
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+
     }
 
 
