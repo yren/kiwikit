8c3dde755c729bb8bd2568a58a106a1f977614f8
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-10-20 06:43:56 +0800

    update

--- src/test/java/me/lokvin/kiwi/kiwikit/base/PlatformsTest.java
+++ src/test/java/me/lokvin/kiwi/kiwikit/base/PlatformsTest.java
@@ -10,5 +10,11 @@ public class PlatformsTest {
         System.out.println("OS_NAME: " + Platforms.OS_NAME);
         System.out.println("OS_VERSION: " + Platforms.OS_VERSION);
         System.out.println("OS_ARCH: " + Platforms.OS_ARCH);
+        System.out.println("JAVA_SPECIFICATION_VERSION: " + Platforms.JAVA_SPECIFICATION_VERSION);
+        System.out.println("JAVA_VERSION: " + Platforms.JAVA_VERSION);
+        System.out.println("JAVA_HOME: " + Platforms.JAVA_HOME);
+        System.out.println("USER_HOME: " + Platforms.USER_HOME);
+        System.out.println("TMP_DIR: " + Platforms.TMP_DIR);
+        System.out.println("WORKING_DIR: " + Platforms.WORKING_DIR);
     }
 }
