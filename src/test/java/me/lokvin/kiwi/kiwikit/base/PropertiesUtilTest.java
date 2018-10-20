7fc8af2261951866db74f3aab538630745e4cd97
Yufei Ren <yufei.ren@thomsonreuters.com>
2018-10-21 01:10:30 +0800

    update

--- src/test/java/me/lokvin/kiwi/kiwikit/base/PropertiesUtilTest.java
+++ src/test/java/me/lokvin/kiwi/kiwikit/base/PropertiesUtilTest.java
@@ -16,10 +16,19 @@ public class PropertiesUtilTest {
 
     @Test
     public void testLoadFromString() {
-        String content = "max: 100\n"
-                + "min: 1";
+        String content = "max=100\n"
+                + "min=1";
         Properties p = PropertiesUtil.loadFromString(content);
         Assert.assertEquals("100", p.getProperty("max"));
         Assert.assertEquals("1", p.getProperty("min"));
     }
+
+    @Test
+    public void testLoadProperties() {
+        Properties p = PropertiesUtil.loadFromFile("application.properties");
+        Assert.assertTrue(PropertiesUtil.getBoolean(p, "obj.flag", false));
+        Assert.assertFalse("not exist obj.openFlag", PropertiesUtil.getBoolean(p, "obj.openFlag", false));
+        Assert.assertTrue("obj.time = 1",PropertiesUtil.getInt(p, "obj.time", 0) == 1);
+    }
+
 }
